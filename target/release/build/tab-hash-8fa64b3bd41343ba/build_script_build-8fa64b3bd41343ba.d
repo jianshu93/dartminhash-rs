ae0c38fa90777178545c412a8c6a0803a8d40f2b
jianshu93 <jansuechao@gmail.com>
2025-08-03 16:07:50 -0700

    update readme

--- target/release/build/tab-hash-8fa64b3bd41343ba/build_script_build-8fa64b3bd41343ba.d
+++
@@ -1,5 +0,0 @@
-/Users/jianshuzhao/dartminhash-rs/target/release/build/tab-hash-8fa64b3bd41343ba/build_script_build-8fa64b3bd41343ba.d: /Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tab-hash-0.3.0/build.rs
-
-/Users/jianshuzhao/dartminhash-rs/target/release/build/tab-hash-8fa64b3bd41343ba/build_script_build-8fa64b3bd41343ba: /Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tab-hash-0.3.0/build.rs
-
-/Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tab-hash-0.3.0/build.rs:
--- target/release/deps/mt19937-e1c4995299333b69.d
+++
@@ -1,7 +0,0 @@
-/Users/jianshuzhao/dartminhash-rs/target/release/deps/mt19937-e1c4995299333b69.d: /Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/mt19937-3.1.0/src/lib.rs
-
-/Users/jianshuzhao/dartminhash-rs/target/release/deps/libmt19937-e1c4995299333b69.rlib: /Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/mt19937-3.1.0/src/lib.rs
-
-/Users/jianshuzhao/dartminhash-rs/target/release/deps/libmt19937-e1c4995299333b69.rmeta: /Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/mt19937-3.1.0/src/lib.rs
-
-/Users/jianshuzhao/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/mt19937-3.1.0/src/lib.rs:
