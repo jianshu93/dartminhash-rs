6cb9aef5dfd416cc249bd0b1b0a672a9d60d2ca3
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-08-15 13:13:08 -0700

    update readme

--- target/package/dartminhash-0.1.1/target/debug/build/tab-hash-557275610a8a42fd/build_script_build-557275610a8a42fd.d
+++
@@ -1,5 +0,0 @@
-/Users/jiz322/Github/dartminhash-rs/target/package/dartminhash-0.1.1/target/debug/build/tab-hash-557275610a8a42fd/build_script_build-557275610a8a42fd.d: /Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tab-hash-0.3.0/build.rs
-
-/Users/jiz322/Github/dartminhash-rs/target/package/dartminhash-0.1.1/target/debug/build/tab-hash-557275610a8a42fd/build_script_build-557275610a8a42fd: /Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tab-hash-0.3.0/build.rs
-
-/Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tab-hash-0.3.0/build.rs:
--- target/package/dartminhash-0.1.1/target/debug/deps/cfg_if-b7a756bc843dd0d0.d
+++
@@ -1,7 +0,0 @@
-/Users/jiz322/Github/dartminhash-rs/target/package/dartminhash-0.1.1/target/debug/deps/cfg_if-b7a756bc843dd0d0.d: /Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
-
-/Users/jiz322/Github/dartminhash-rs/target/package/dartminhash-0.1.1/target/debug/deps/libcfg_if-b7a756bc843dd0d0.rlib: /Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
-
-/Users/jiz322/Github/dartminhash-rs/target/package/dartminhash-0.1.1/target/debug/deps/libcfg_if-b7a756bc843dd0d0.rmeta: /Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
-
-/Users/jiz322/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs:
